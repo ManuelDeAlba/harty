c2ef20bdfe31479bb62928bd63d86289af9daeb4
dneb8 <107234871+dneb8@users.noreply.github.com>
2023-10-18 22:26:34 -0600

    Revert "Revert "Scripts de js arreglados????""
    This reverts commit c761400bac67ca686e17b119810326701d99c6cb.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import React, { useEffect } from 'react';
 
 // Contextos
 import AuthProvider from "./context/AuthProvider";
@@ -15,12 +16,73 @@ import FormularioIniciarSesion from "./pages/FormularioIniciarSesion";
 import Navbar from "./components/Navbar";
 import Protegido from "./components/Protegido";
 import Head from './components/Head';
-import ScriptImports from './components/ScriptImports';
+
 
 function App(){
+    useEffect(() => {
+        function loadScripts(scripts, callback) {
+            const head = document.getElementsByTagName('head')[0];
+            let loadedCount = 0;
+          
+            function loadScript(index) {
+              if (index >= scripts.length) {
+                // Todos los scripts se han cargado
+                callback();
+                return;
+              }
+          
+              const script = document.createElement('script');
+              script.type = 'text/javascript';
+              script.src = scripts[index];
+              script.async = true;
+          
+              script.onload = () => {
+                loadedCount++;
+                if (loadedCount === scripts.length) {
+                  // Cuando se han cargado todos los scripts, llamamos al callback
+                  callback();
+                } else {
+                  // Cargamos el siguiente script
+                  loadScript(index + 1);
+                }
+              };
+          
+              script.onerror = (error) => {
+                // Manejo de errores si el script no se carga correctamente
+                console.error('Error al cargar el script:', error);
+                // Continuamos cargando el siguiente script
+                loadScript(index + 1);
+              };
+          
+              head.appendChild(script);
+            }
+          
+            // Comenzamos cargando el primer script
+            loadScript(0);
+          }
+    
+        const scriptsToLoad = [
+          'assets/js/jquery-1.11.3.min.js',
+          'assets/bootstrap/js/bootstrap.min.js',
+          'assets/js/jquery.countdown.js',
+          'assets/js/jquery.isotope-3.0.6.min.js',
+          'assets/js/waypoints.js',
+          'assets/js/owl.carousel.min.js',
+          'assets/js/jquery.magnific-popup.min.js',
+          'assets/js/jquery.meanmenu.min.js',
+          'assets/js/sticker.js',
+          'assets/js/main.js'
+        ];
+    
+        loadScripts(scriptsToLoad, () => {
+          // Los scripts se han cargado, puedes realizar acciones adicionales si es necesario
+        });
+      }, []);
+
     return(
         <AuthProvider>
             <Head />
+           
             <Router>
                 <Navbar />
                 <Toaster />
@@ -37,7 +99,6 @@ function App(){
                     <Route path="/registrarse" element={<FormularioRegistrarse />} />
                     <Route path="/iniciar-sesion" element={<FormularioIniciarSesion />} />
                 </Routes>
-                <ScriptImports />
             </Router>
         </AuthProvider>
     )
