c761400bac67ca686e17b119810326701d99c6cb
dneb8 <107234871+dneb8@users.noreply.github.com>
2023-10-18 22:25:05 -0600

    Revert "Scripts de js arreglados????"
    This reverts commit 4a06fda8f621fd114e114892a9ee5adb5546c80c.

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,37 +1,24 @@
+// HeadComponent.jsx
 import React from 'react';
-import { Helmet } from 'react-helmet';
 
-// HeadComponent para usar como head en las páginas: <Head/>
 function Head() {
   return (
     <div>
-      <Helmet>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com" />
-        <link rel="shortcut icon" type="image/png" href="assets/img/favicon.png" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="assets/css/all.min.css" />
-        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="assets/css/owl.carousel.css" />
-        <link rel="stylesheet" href="assets/css/magnific-popup.css" />
-        <link rel="stylesheet" href="assets/css/animate.css" />
-        <link rel="stylesheet" href="assets/css/meanmenu.min.css" />
-        <link rel="stylesheet" href="assets/css/main.css" />
-        <link rel="stylesheet" href="assets/css/responsive.css" />
-        <script type="text/javascript" src="assets/js/jquery-1.11.3.min.js"></script>
-        <script type="text/javascript" src="assets/bootstrap/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="assets/js/jquery.countdown.js"></script>
-        <script type="text/javascript" src="assets/js/jquery.isotope-3.0.6.min.js"></script>
-        <script type="text/javascript" src="assets/js/waypoints.js"></script>
-        <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
-        <script type="text/javascript" src="assets/js/jquery.magnific-popup.min.js"></script>
-        <script type="text/javascript" src="assets/js/jquery.meanmenu.min.js"></script>
-        <script type="text/javascript" src="assets/js/sticker.js"></script>
-        <script type="text/javascript" src="assets/js/main.js"></script>
-      </Helmet>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Responsive Bootstrap4 Shop Template, Created by Imran Hossain from https://imransdesign.com" />
+      <link rel="shortcut icon" type="image/png" href="assets/img/favicon.png" />
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap" rel="stylesheet" />
+      <link rel="stylesheet" href="assets/css/all.min.css" />
+      <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="assets/css/owl.carousel.css" />
+      <link rel="stylesheet" href="assets/css/magnific-popup.css" />
+      <link rel="stylesheet" href="assets/css/animate.css" />
+      <link rel="stylesheet" href="assets/css/meanmenu.min.css" />
+      <link rel="stylesheet" href="assets/css/main.css" />
+      <link rel="stylesheet" href="assets/css/responsive.css" />
     </div>
   );
 }
