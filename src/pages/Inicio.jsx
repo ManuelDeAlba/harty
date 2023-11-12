28606ec933afad587c74bf9d0286545e3763c6b8
dneb8 <107234871+dneb8@users.noreply.github.com>
2023-11-11 18:25:33 -0600

    Revert "Revert "Merge branch 'main' into deneb""
    This reverts commit 45891212572206e1507b6c73e3c370c51af42e0d.

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Slide } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
 import { Link } from "react-router-dom";
 
-const numSlides = [0, 1, 2];
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
 const fondos = ["/assets/img/hero-bg.jpg", "/assets/img/hero-bg-2.png", "/assets/img/hero-bg-3.png"];
 
 const Inicio = () => {
     return (
         <header className="header__inicio header">
-            <Slide className="header__slider" images={numSlides} autoPlay={true} autoPlayInterval={1000}>
-                <div className="header__slide" style={{ backgroundImage: `url(${fondos[0]})` }}>
+            <Swiper
+                className='header__slider'
+                modules={[Autoplay]}
+                slidesPerView={1}
+                autoplay={{ delay: 5000 }}
+                speed={1000}
+                loop
+            >
+                <SwiperSlide className="header__slide" style={{ backgroundImage: `url(${fondos[0]})` }}>
                     <div className="header__contenedor contenedor">
                         <p className="header__texto">Fiestas Inolvidables</p>
                          <h1 className="header__titulo">Encuentra tu lugar perfecto</h1>
@@ -19,8 +27,8 @@ const Inicio = () => {
                             <Link className='header__cta header__cta--borde boton' to="/publicar-terraza">Publica tu Espacio</Link>
                         </div>
                     </div>
-                </div>
-                <div className="header__slide" style={{ backgroundImage: `url(${fondos[1]})` }}>
+                </SwiperSlide>
+                <SwiperSlide className="header__slide" style={{ backgroundImage: `url(${fondos[1]})` }}>
                     <div className="header__contenedor header__contenedor--2 contenedor">
                         <p className="header__texto">Terrazas Exclusivas</p>
                         <h1 className="header__titulo">Las mejores terrazas con alberca para tu Fiesta</h1>
@@ -29,8 +37,8 @@ const Inicio = () => {
                            <Link className='header__cta header__cta--borde boton' to="/publicar-terraza">Publica tu Terraza</Link>
                         </div>
                     </div>
-                </div>
-                <div className="header__slide" style={{ backgroundImage: `url(${fondos[2]})` }}>
+                </SwiperSlide>
+                <SwiperSlide className="header__slide" style={{ backgroundImage: `url(${fondos[2]})` }}>
                     <div className="header__contenedor header__contenedor--3 contenedor">
                         <p className="header__texto">Celebra con Amigos</p>
                         <h1 className="header__titulo">Espacios íntimos para eventos </h1>
@@ -39,8 +47,8 @@ const Inicio = () => {
                             <Link className='header__cta header__cta--borde boton' to="/publicar-terraza">Publica tu Lugar</Link>
                         </div>
                     </div>
-                </div>
-            </Slide>
+                </SwiperSlide>
+            </Swiper>
         </header>
     );
 };
